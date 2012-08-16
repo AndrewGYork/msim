875afb6d75d0199c04539b6c730647fa49c4cc21
Andrew G York <Andrew.G.York@gmail.com>
2012-08-16 19:14:53 +0000

    Code for controlling the gooch AOTF got a little more polished

--- hardware_control/gooch.py
+++ hardware_control/gooch.py
@@ -2,46 +2,80 @@ import serial
 
 class AOTF:
     def __init__(self):
+        print 'initializing the AOTF'
         try:
             self.ser = serial.Serial(port=3) #Have to pick the right port
         except:
-            raise UserWarning("Can't open the gooch")
+            raise UserWarning("Can't open the gooch. Is it on?")
         for i in range(8):
-            self.set_amplitude(channel=i+1, amplitude=0)
-            self.off(channel=i+1)
-        self.set_frequency(channel=1, frequency=115.5)
-####  Channel 1 is the 405nm channel
-        self.set_frequency(channel=2, frequency=86.55)
-        self.set_amplitude(channel=2, amplitude=1023)
-        self.on(channel=2)
-####  Channel 2 is the 488nm channel
+            self.set_amplitude(color=i+1, amplitude=0, verbose=False)
+            self.off(color=i+1, verbose=False)
+        self.set_frequency(color=1, frequency=116.1)
+        self.set_amplitude(color=1, amplitude=1023, verbose=False)
+        self.on(color='405', verbose=False)
+        self.set_frequency(color=2, frequency=87.69)
+        self.set_amplitude(color=2, amplitude=1023, verbose=False)
+        self.on(color='488', verbose=False)
+        """
+        The Gooch has 8 channels
+        Channel=1 is the 405nm laser channel
+        Channel=2 is the 488nm laser channel
+        """
+        print 'AOTF is cracking'
         return None
 
-
-    def set_frequency(self, channel, frequency):
-##        assert(channel == 1 or channel == 2 or channel == 3)
-##        try:
-##            channel = int(channel)
-##        except ValueError:
-##            raise UserWarning("'channel' has to be an integer, jackass")
+    def color_to_channel(self, color):
+        if color == '488':
+            return 2
+        elif color == '405':
+            return 1
+        elif color == 'all':
+            return 0
+        else:
+            try:
+                channel = int(color)
+                assert channel in range(9)
+                return channel
+            except:
+                print "AOTF color not understood"
+                print color
+                raise
+    
+    def set_frequency(self, color, frequency):
+        channel=self.color_to_channel(color)
         self.ser.write('ch%i\r\n'%(channel))
         self.ser.write('fr %0.3f\r\n'%(frequency))
         return None
 
-    def set_amplitude(self, channel, amplitude):
+    def set_amplitude(self, color, amplitude, verbose=True):
+        if verbose:
+            print 'Setting the amplitude of the', color,
+            print 'channel of the AOTF to', amplitude
+        channel=self.color_to_channel(color)
         self.ser.write('ch%i\r\n'%(channel))
         self.ser.write('am %i\r\n'%(amplitude))
         return None
 
-    def on(self, channel):
+    def on(self, color, verbose=True):
+        if verbose:
+            print 'Turning on the', color,  'channel of the AOTF'
+        channel=self.color_to_channel(color)
         self.ser.write('ch%i\r\n'%(channel))
         self.ser.write('on\r\n')
         return None
 
-    def off(self, channel=0):
+    def off(self, color, verbose=True):
+        if verbose:
+            print 'Turning off the', color,  'channel of the AOTF'
+        channel=self.color_to_channel(color)
         self.ser.write('ch%i\r\n'%(channel))
         self.ser.write('off\r\n')
         return None
+
+##    def off_global(self, color=0):
+##        self.ser.write('ch%i\r\n'%(channel))
+##        self.ser.write('off\r\n')
+##        return None
 
     def flushbuffers(self):
         self.ser.flush(self)
@@ -50,24 +84,29 @@ class AOTF:
         return None
     
     def close(self):
-##        self.set_amplitude(channel=0, amplitude=0)
-        self.set_frequency(channel=2, frequency=86.55)
-        self.set_amplitude(channel=2, amplitude=1023)
-        self.on(channel=2)
+        self.set_amplitude('all', amplitude=0, verbose=False)
+        self.set_frequency('488', frequency=87.69)
+        self.set_amplitude('488', amplitude=1023, verbose=False)
+        self.on(color='488', verbose=False)
+        self.set_frequency('405', frequency=116.1)
+        self.set_amplitude('405', amplitude=1023, verbose=False)
+        self.on(color='405', verbose=False)
         self.ser.close()
+        print 'AOTF is now closed'
         return None
 
 if __name__ == '__main__':
-    import time
+##    import time
     a = AOTF()
-    a.off(channel=2)
-    time.sleep(2)
-##    a.set_amplitude(channel=1, amplitude=10)
-##    a.set_frequency(channel=1, frequency=86.55)
-##    a.on(channel=1)
+##    a.off('488')
+####    time.sleep(2)
+####    a.set_amplitude(channel=1, amplitude=1023)
+####    a.set_frequency(channel=1, frequency=116.1)
+##    a.off('488')
 ##
-##    for i in range(800, 900):
+##    for i in range(1130, 1190):
 ##        print i
 ##        a.set_frequency(channel=1, frequency=i*0.1)
-##        time.sleep(.01)
+##        time.sleep(1)
+##    a.on('488')
     a.close()

221e88738122d6641d4576ed52a33b62c3db482c
Andrew G York <Andrew.G.York@gmail.com>
2013-04-30 23:07:43 +0000

    A few plodding additions to msim_gui.py

--- hardware_control/msim_gui.py
+++ hardware_control/msim_gui.py
@@ -1,7 +1,11 @@
+import os
 ##import time
 import logging
+import ConfigParser
 import multiprocessing as mp
 import Tkinter as tk
+import tkMessageBox
+import tkFileDialog
 ##import numpy as np
 from image_data_pipeline import Image_Data_Pipeline
 
@@ -18,6 +22,9 @@ class GUI:
             num_buffers=5,
             buffer_shape=(224, 480, 480))
         self.root = tk.Tk()
+        self.root.iconbitmap(default='microscope.ico')
+        self.root.report_callback_exception = self.report_callback_exception
+        self.root.title("MSIM controls")
 
         self.menubar = tk.Menu(self.root)
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
@@ -26,14 +33,48 @@ class GUI:
         self.settingsmenu = tk.Menu(self.menubar, tearoff=0)
         self.settingsmenu.add_command(
             label="Display", command=self.open_display_settings_window)
+        self.settingsmenu.add_command(
+            label="Brightfield mode", command=self.open_brightfield_window)
+        self.settingsmenu.add_command(
+            label="Emission filters", command=self.open_filter_window)
         self.menubar.add_cascade(label="Settings", menu=self.settingsmenu)
         self.root.config(menu=self.menubar)
 
+        self.lasers = ['561', '488']
+        self.emission_filters = {}
+        for c in self.lasers:
+            self.emission_filters[c] = tk.StringVar()
+            self.emission_filters[c].set('Filter 3')
+
+        self.laser_power = {}
+        self.laser_on = {}
+        self.lake_info = {}
+        frame = tk.Frame(self.root, bd=4, relief=tk.SUNKEN)
+        frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        a = tk.Label(frame, text='Snap settings:')
+        a.pack(side=tk.TOP)
+        for c in self.lasers:
+            subframe = tk.Frame(frame)
+            subframe.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+            a = tk.Label(subframe, text='Laser power:\n'+c+' nm (%)')
+            a.pack(side=tk.LEFT)
+            self.laser_power[c] = Scale_Spinbox(
+                subframe, from_=0.1, to=100, increment=0.1, initial_value=100)
+            self.laser_power[c].spinbox.config(width=5)
+            self.laser_power[c].pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+            self.laser_on[c] = tk.IntVar()
+            a = tk.Checkbutton(subframe, text='',
+                               variable=self.laser_on[c])
+            if c == '488':
+                self.laser_on[c].set(1)
+            else:
+                self.laser_on[c].set(0)
+            a.pack(side=tk.LEFT)
+            
+
+
         frame = tk.Frame(self.root)
         frame.pack(side=tk.TOP)
-
-        a = tk.Button(frame, text="QUIT", fg="red", command=frame.quit)
-        a.pack(side=tk.LEFT)
         
         a.pack(side=tk.LEFT)
         a = tk.Button(
@@ -41,14 +82,15 @@ class GUI:
             command=self.load_buffers)
         a.pack(side=tk.LEFT)
         
-##        self.file_num = 0
-##        a = tk.Button(
-##            frame, text="Load buffers and save",
-##            command=self.load_and_save)
-##        a.pack(side=tk.LEFT)
-##
+        self.file_num = 0
+        a = tk.Button(
+            frame, text="Load buffers and save",
+            command=self.load_and_save)
+        a.pack(side=tk.LEFT)
+
         self.data_pipeline.set_display_intensity_scaling(
             'median_filter_autoscale', display_min=0, display_max=0)
+        self.root.after(50, self.load_config)
         self.root.mainloop()
         self.data_pipeline.close()
         return None
@@ -59,19 +101,19 @@ class GUI:
             len(self.data_pipeline.idle_buffers))
         return None
 
-##    def load_and_save(self):
-##        self.data_pipeline.collect_data_buffers()
-##        num_buffers = len(self.data_pipeline.idle_buffers)
-##        file_info = []
-##        for b in self.data_pipeline.idle_buffers:
-##            file_info.append(
-##                {'buffer_number': b,
-##                 'outfile': 'image_%06i.tif'%(self.file_num)
-##                 })
-##            self.file_num += 1
-##        self.data_pipeline.load_data_buffers(
-##            num_buffers, file_saving_info=file_info)
-##        return None
+    def load_and_save(self):
+        self.data_pipeline.collect_data_buffers()
+        num_buffers = len(self.data_pipeline.idle_buffers)
+        file_info = []
+        for b in self.data_pipeline.idle_buffers:
+            file_info.append(
+                {'buffer_number': b,
+                 'outfile': 'image_%06i.tif'%(self.file_num)
+                 })
+            self.file_num += 1
+        self.data_pipeline.load_data_buffers(
+            num_buffers, file_saving_info=file_info)
+        return None
 
     def open_display_settings_window(self):
         try:
@@ -81,6 +123,67 @@ class GUI:
         self.display_settings_window.root.lift()
         self.display_settings_window.root.focus_force()
         return None
+
+    def open_brightfield_window(self):
+        """TODO"""
+        return None
+
+    def open_filter_window(self):
+        try:
+            self.filter_window.root.config()
+        except (AttributeError, tk.TclError):
+            self.filter_window = Filter_Window(self)
+        self.filter_window.root.lift()
+        self.filter_window.root.focus_force()
+        return None
+
+    def report_callback_exception(self, *args):
+        import traceback
+        err = traceback.format_exception(*args)
+        with open(os.path.join(
+            os.getcwd(),
+            'error_log_gui.txt'), 'ab') as error_log:
+            for e in err:
+                error_log.write(e + os.linesep)
+            error_log.write(os.linesep*2)
+        tkMessageBox.showerror(
+            'Exception',
+            'An exception occured. ' +
+            'Read "error_log.txt" in:\n' +
+            repr(os.getcwd()) +
+            '\nfor details."')
+        return None
+
+    def load_config(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(os.path.join(os.getcwd(), 'config.ini'))
+        while True:
+            try:
+                imagej_path = self.config.get('ImageJ', 'path')
+                assert os.path.basename(imagej_path).lower() == 'imagej.exe'
+                break
+            except ConfigParser.NoSectionError:
+                self.config.add_section('ImageJ')
+            except ConfigParser.NoOptionError:
+                imagej_path = str(os.path.normpath(tkFileDialog.askopenfilename(
+                    title="Where is the ImageJ executable?",
+                    filetypes=[('Executable', '.exe')],
+                    defaultextension='.raw',
+                    initialdir=os.getcwd(),
+                    initialfile='ImageJ.exe'))) #Careful about Unicode here!
+                self.config.set('ImageJ', 'path', imagej_path)
+            except AssertionError:
+                try_again = tkMessageBox.askretrycancel(
+                    'ImageJ executable is weird',
+                    'Try again?')
+                if try_again:
+                    self.config.remove_option('ImageJ', 'path')
+                else:
+                    break
+        with open('config.ini', 'w') as configfile:
+            self.config.write(configfile)
+        return None
+
         
 class Display_Settings_Window:
     def __init__(self, parent):
@@ -149,9 +252,6 @@ class Display_Settings_Window:
         """
         scaling, display_min, display_max = (
             self.parent.data_pipeline.get_display_intensity_scaling())
-        print repr(scaling)
-        print repr(display_min)
-        print repr(display_max)
         self.scaling.set(scaling)
         self.display_min.set(int(display_min), update_trigger=False)
         self.display_max.set(int(display_max), update_trigger=False)
@@ -160,11 +260,31 @@ class Display_Settings_Window:
     def update_scaling(self):
         scaling, display_min, display_max = (
             self.parent.data_pipeline.get_display_intensity_scaling())
-        if scaling != 'linear':
+        if scaling == 'autoscale' or scaling == 'median_filter_autoscale':
             self.display_min.set(display_min)
             self.display_max.set(display_max)
         self.root.after(400, self.update_scaling)
         return None
+
+class Filter_Window:
+    def __init__(self, parent):
+        self.parent = parent
+        self.root = tk.Toplevel(parent.root)
+        self.root.wm_title("Emission filter settings")
+        self.root.bind("<Escape>", lambda x: self.root.destroy())
+
+        a = tk.Label(self.root, text="Emission filter settings:")
+        a.pack(side=tk.TOP)
+        for c in self.parent.lasers:
+            frame = tk.Frame(self.root)
+            frame.pack(side=tk.TOP, fill=tk.BOTH)
+            a = tk.Label(master=frame, text=c + ' nm laser\nemission filter:')
+            a.pack(side=tk.LEFT)
+            a = tk.OptionMenu(frame, self.parent.emission_filters[c],
+                              *['Filter %i'%i for i in range(1, 7)])
+            a.pack(side=tk.LEFT)
+        return None
+
     
 class Scale_Spinbox:
     def __init__(self, master, from_, to, increment=1, initial_value=None):

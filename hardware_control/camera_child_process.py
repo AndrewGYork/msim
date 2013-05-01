6edc952d7ef4b705f073289730d2448511bacc10
Andrew G York <Andrew.G.York@gmail.com>
2013-05-01 22:05:56 +0000

    camera_child_process.py now allows some external commands, including the ability to fill only part of a buffer.

--- hardware_control/camera_child_process.py
+++ hardware_control/camera_child_process.py
@@ -36,7 +36,20 @@ def camera_child_process(
     camera.arm(num_buffers=3)
     camera._prepare_to_record_to_memory()
     preframes = 0
+    frames_per_acquisition = 'buffer_size'
     while True:
+        if commands.poll():
+            cmd, args = commands.recv()
+            if cmd == 'apply_settings':
+                settings = camera.apply_settings(**args)
+                camera.arm()
+                commands.send(settings)
+            elif cmd == 'get_settings':
+                commands.send(camera.get_settings(**args))
+            elif cmd == 'frames_per_acquisition':
+                frames_per_acquisition = args['frames_per_acquisition']
+                commands.send(frames_per_acquisition)
+            continue
         try:
             process_me = input_queue.get_nowait()
         except Queue.Empty:
@@ -50,8 +63,13 @@ def camera_child_process(
             with data_buffers[process_me].get_lock():
                 a = np.frombuffer(data_buffers[process_me].get_obj(),
                                   dtype=np.uint16).reshape(buffer_shape)
+                if frames_per_acquisition is 'buffer_size':
+                    num_images = a.shape[0] + preframes
+                else:
+                    num_images = frames_per_acquisition + preframes
+                    a[frames_per_acquisition:, :, :].fill(0)
                 camera.record_to_memory(
-                    num_images=a.shape[0] + preframes,
+                    num_images=num_images,
                     preframes=preframes,
                     out=a)
             info("end buffer %i"%(process_me))

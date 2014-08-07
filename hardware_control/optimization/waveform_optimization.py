29b5cd3fd9eeb9c20e96972c99cd2e26dffef55b
Andrew G York <Andrew.G.York@gmail.com>
2014-08-07 16:20:39 +0000

    Waveform optimization update

--- hardware_control/optimization/waveform_optimization.py
+++ hardware_control/optimization/waveform_optimization.py
@@ -28,20 +28,20 @@ simple_tif.array_to_tif(
     impulse_response.astype(np.float32).reshape(impulse_response.size, 1, 1),
     'impulse_response.tif')
 
-camera_exposures = 4
-live_facets = 4
-flyback_facets = 1
-len_ramp = 42
-len_facet = 250
-len_undefined = len_facet - len_ramp
-len_linker = len_facet
-len_voltage = (camera_exposures *
-               (live_facets + flyback_facets) *
-               (len_undefined + len_ramp))
-len_response = (camera_exposures * live_facets * len_ramp +
-                len_linker)
-len_flyback = flyback_facets * len_facet
-assert impulse_response.size >= (len_voltage + len_linker)
+camera_exposures = 2 #Has to be an even number
+sweeps_per_exposure = 4
+len_warmup = 200
+len_sweep = 79 #DAQ pix
+len_ramp = 60 #DAQ pix
+len_undefined = len_sweep - len_ramp
+len_voltage = (len_warmup +
+               (camera_exposures *
+                sweeps_per_exposure *
+                len_sweep))
+len_response = (camera_exposures *
+               sweeps_per_exposure *
+               len_ramp)
+assert impulse_response.size >= len_voltage
 
 def H(voltage):
     """
@@ -52,8 +52,9 @@ def H(voltage):
     response = np.zeros(len_response)
     voltage_cursor = 0
     response_cursor = 0
+    voltage_cursor += len_warmup #Warmup is undefined
     for ex in range(camera_exposures):
-        for f in range(live_facets):
+        for s in range(sweeps_per_exposure):
             voltage_cursor += len_undefined
             response[response_cursor:
                      response_cursor + len_ramp
@@ -61,11 +62,6 @@ def H(voltage):
                                        voltage_cursor + len_ramp]
             voltage_cursor += len_ramp
             response_cursor += len_ramp
-        voltage_cursor += len_flyback
-    response[response_cursor:
-             response_cursor + len_linker
-             ] = blurred_voltage[voltage_cursor:
-                                 voltage_cursor + len_linker]
     return response
     
 def H_transpose(residual):
@@ -75,11 +71,12 @@ def H_transpose(residual):
     right dimensions to correct the estimate.
     """
     assert residual.size == len_response
-    correction = np.zeros(len_voltage + len_linker, dtype=np.float64) #This pads
+    correction = np.zeros(len_voltage, dtype=np.float64) #This pads
     residual_cursor = 0
     correction_cursor = 0
+    correction_cursor += len_warmup #Warmup is undefined
     for ex in range(camera_exposures):
-        for f in range(live_facets):
+        for s in range(sweeps_per_exposure):
             correction_cursor += len_undefined
             correction[correction_cursor:
                        correction_cursor + len_ramp
@@ -87,43 +84,80 @@ def H_transpose(residual):
                                     residual_cursor + len_ramp]
             residual_cursor += len_ramp
             correction_cursor += len_ramp
-        correction_cursor += len_flyback
-    correction[correction_cursor:
-               correction_cursor + len_linker
-               ] = residual[residual_cursor:
-                            residual_cursor + len_linker]
     blurred_correction = np.convolve(correction, #This blurs
                                      impulse_response[::-1]
-                                     )[-len_linker - len_voltage:
-                                       -len_linker] #This crops!
+                                     )[-len_voltage:] #This crops!
     return blurred_correction
 
-input_voltage = np.zeros(len_voltage, dtype=np.float64)
-desired_result = np.zeros(len_response, dtype=np.float64)
-time = np.zeros_like(desired_result)
+input_voltage_sample = np.zeros(len_voltage, dtype=np.float64)
+input_voltage_camera = np.zeros(len_voltage, dtype=np.float64)
+desired_result_sample = np.zeros(len_response, dtype=np.float64)
+desired_result_camera = np.zeros(len_response, dtype=np.float64)
+time = np.zeros_like(desired_result_sample)
 rise_per_ramp = 1
 input_voltage_cursor = 0
 desired_result_cursor = 0
+"""
+if camera exposure is even, need to switch sawtooth scan direction
+and direction rescan direction across camera
+"""
+input_voltage_cursor += len_warmup #Warmup is undefined
 for ex in range(camera_exposures):
-    for f in range(live_facets):
-        input_voltage[
+    for s in range(sweeps_per_exposure):
+        """
+        Each sweep starts with an undefined region
+        """
+        if s == 0: #First sample sweep starts flat
+            final_value = input_voltage_sample[input_voltage_cursor - 1]
+        else: #Remaining sample sweeps start with flybacks
+            if ex % 2 == 0: #Even exposures flyback down
+                final_value = 0
+            else: #Odd exposures flyback up
+                final_value = rise_per_ramp
+        input_voltage_sample[
             input_voltage_cursor:
             input_voltage_cursor + len_undefined
             ] = np.linspace(
-                input_voltage[input_voltage_cursor - 1], #Hacky :(
-                input_voltage[input_voltage_cursor - 1],
+                input_voltage_sample[input_voltage_cursor - 1], #Hacky :(
+                final_value,
+                len_undefined)
+        input_voltage_camera[ #All camera sweeps start flat
+            input_voltage_cursor:
+            input_voltage_cursor + len_undefined
+            ] = np.linspace(
+                input_voltage_camera[input_voltage_cursor - 1], #Hacky :(
+                input_voltage_camera[input_voltage_cursor - 1],
                 len_undefined)
         input_voltage_cursor += len_undefined
-        if f == 0:
-            start_point = 0
-        else:
-            start_point = desired_result[desired_result_cursor - 1]
-        desired_result[
+        """
+        Now we execute the defined portion of the sweep.
+
+        First, desired results for the defined region:
+        """
+        if ex % 2 == 0: #Even sweeps start at zero and sweep up
+            start_value = 0
+            rise = rise_per_ramp
+        else: #Odd sweeps start at 'rise_per_ramp' and sweep down
+            start_value = rise_per_ramp
+            rise = - rise_per_ramp
+        desired_result_sample[
             desired_result_cursor:
             desired_result_cursor + len_ramp
             ] = np.linspace(
-                start_point,
-                start_point + rise_per_ramp,
+                start_value,
+                start_value + rise,
+                len_ramp)
+        start_value = desired_result_camera[max(desired_result_cursor - 1, 0)]
+        if ex % 2 == 0: #Even sweeps sweep up
+            rise = rise_per_ramp
+        else: #Odd sweeps sweep down
+            rise = -rise_per_ramp
+        desired_result_camera[
+            desired_result_cursor:
+            desired_result_cursor + len_ramp
+            ] = np.linspace(
+                start_value,
+                start_value + rise,
                 len_ramp)
         time[
             desired_result_cursor:
@@ -131,70 +165,72 @@ for ex in range(camera_exposures):
             ] = np.arange(input_voltage_cursor,
                           input_voltage_cursor + len_ramp)
         desired_result_cursor += len_ramp
-
-        input_voltage[
+        """
+        Now input voltages for the defined region of the sweep:
+        """
+        if ex % 2 == 0: #Even exposures sweep up
+            rise = rise_per_ramp
+        else: #Odd exposures sweep down
+            rise = -rise_per_ramp
+        input_voltage_sample[
             input_voltage_cursor:
             input_voltage_cursor + len_ramp
             ] = np.linspace(
-                input_voltage[input_voltage_cursor - 1],
-                input_voltage[input_voltage_cursor - 1] + rise_per_ramp,
+                input_voltage_sample[input_voltage_cursor - 1],
+                input_voltage_sample[input_voltage_cursor - 1] + rise,
                 len_ramp)
+        input_voltage_camera[
+            input_voltage_cursor:
+            input_voltage_cursor + len_ramp
+            ] = np.linspace(
+                input_voltage_camera[input_voltage_cursor - 1],
+                input_voltage_camera[input_voltage_cursor - 1] + rise,
+                len_ramp)        
         input_voltage_cursor += len_ramp
-    input_voltage[
-        input_voltage_cursor:
-        input_voltage_cursor + len_flyback
-        ] = np.linspace(
-            input_voltage[input_voltage_cursor - 1],
-            0,
-            len_flyback)
-    input_voltage_cursor += len_flyback
-desired_result[
-    desired_result_cursor:
-    desired_result_cursor + len_linker
-    ] = 0
-time[
-    desired_result_cursor:
-    desired_result_cursor + len_linker
-    ] = np.arange(input_voltage_cursor,
-                  input_voltage_cursor + len_linker)
-desired_result_cursor += len_linker
-
-input_voltage[
-    input_voltage_cursor:
-    input_voltage_cursor + len_linker
-    ] = 0
-input_voltage_cursor += len_linker
 
 ##plt.figure()
-##plt.plot(input_voltage)
-##plt.plot(time, desired_result, '.')
-##plt.plot(time, H(input_voltage), '.')
+##plt.plot(input_voltage_sample)
+##plt.plot(time, desired_result_sample, '.')
+##plt.plot(time, H(input_voltage_sample), '.')
+##plt.plot(input_voltage_camera)
+##plt.plot(time, desired_result_camera, '.')
+##plt.plot(time, H(input_voltage_camera), '.')
 ##plt.show()
 
-naive_input_voltage = input_voltage.copy()
-print "Loading 'input_voltage.tif'..."
+naive_input_voltage_sample = input_voltage_sample.copy()
+naive_input_voltage_camera = input_voltage_camera.copy()
+print "Loading 'input_voltage_sample.tif' and 'input_voltage_camera.tif'..."
 try:
-    input_voltage = simple_tif.tif_to_array('input_voltage.tif'
-                                                  ).ravel().astype(np.float64)
+    input_voltage_sample = simple_tif.tif_to_array('input_voltage_sample.tif'
+                                                   ).ravel().astype(np.float64)
+    input_voltage_camera = simple_tif.tif_to_array('input_voltage_camera.tif'
+                                                   ).ravel().astype(np.float64)
     print "Initial guess loaded"
 except IOError:
-    print "Loading failed. Using naive input voltage as initial guess."
+    print "Loading failed. Using naive input voltages as initial guess."
 
 iterations = 1000000
-regularization = 0.05
+regularization = 0.1
 plt.close('all')
 plt.figure()
 for i in range(iterations):
     print 'Iteration', i
-    expected_result = H(input_voltage)
+    expected_result_sample = H(input_voltage_sample)
+    expected_result_camera = H(input_voltage_camera)
     
-    expected_result_uncropped = np.convolve(
-        input_voltage, impulse_response
-        )[:input_voltage.size + len_linker]
+    expected_result_sample_uncropped = np.convolve(
+        input_voltage_sample, impulse_response
+        )[:input_voltage_sample.size]
+    expected_result_camera_uncropped = np.convolve(
+        input_voltage_camera, impulse_response
+        )[:input_voltage_camera.size]
     plt.clf()
-    plt.plot(input_voltage)
-    plt.plot(time, desired_result, '.', ms=4)
-    plt.plot(expected_result_uncropped, '-')
+    plt.plot(input_voltage_sample)
+    plt.plot(time, desired_result_sample, '.', ms=4)
+    plt.plot(expected_result_sample_uncropped, '-')
+    plt.plot(input_voltage_camera)
+    plt.plot(time, desired_result_camera, '.', ms=4)
+    plt.plot(expected_result_camera_uncropped, '-')
     plt.grid()
     plt.savefig('./comparison/%06i.png'%i)
 ##    plt.clf()
@@ -207,24 +243,41 @@ for i in range(iterations):
 ##    plt.grid()
 ##    plt.savefig('./residuals/%06i.png'%i)
     plt.clf()
-    plt.plot(expected_result - desired_result)
+    plt.plot(expected_result_sample - desired_result_sample)
+    plt.plot(expected_result_camera - desired_result_camera)
     plt.grid()
     plt.savefig('./cropped_residuals/%06i.png'%i)    
 
-    correction_factor = regularization * H_transpose(desired_result -
-                                                     expected_result)
-    input_voltage += correction_factor
+    correction_factor_sample = regularization * H_transpose(
+        desired_result_sample -
+        expected_result_sample)
+    correction_factor_camera = regularization * H_transpose(
+        desired_result_camera -
+        expected_result_camera)
+    input_voltage_sample += correction_factor_sample
+    input_voltage_camera += correction_factor_camera
+
+    input_voltage_sample[input_voltage_sample > 8.3] = 8.3
+    input_voltage_sample[input_voltage_sample < -8.3] = -8.3
 
     for tries in range(10):
         try:
             simple_tif.array_to_tif(
-                input_voltage.astype(np.float32).reshape(
-                    input_voltage.size, 1, 1),
-                'input_voltage.tif')
+                input_voltage_sample.astype(np.float32).reshape(
+                    input_voltage_sample.size, 1, 1),
+                'input_voltage_sample.tif')
             simple_tif.array_to_tif(
-                expected_result_uncropped.astype(np.float32).reshape(
-                    expected_result_uncropped.size, 1, 1),
-                'expected_result.tif')
+                input_voltage_camera.astype(np.float32).reshape(
+                    input_voltage_camera.size, 1, 1),
+                'input_voltage_camera.tif')
+            simple_tif.array_to_tif(
+                expected_result_sample_uncropped.astype(np.float32).reshape(
+                    expected_result_sample_uncropped.size, 1, 1),
+                'expected_result_sample.tif')
+            simple_tif.array_to_tif(
+                expected_result_camera_uncropped.astype(np.float32).reshape(
+                    expected_result_camera_uncropped.size, 1, 1),
+                'expected_result_camera.tif')
             break
         except:
             sleep(0.05)
@@ -233,14 +286,19 @@ for i in range(iterations):
             
 
 plt.clf()
-plt.plot(input_voltage)
-plt.plot(time, desired_result, '.')
-plt.plot(expected_result_uncropped, '.-')
+plt.plot(input_voltage_sample)
+plt.plot(time, desired_result_sample, '.')
+plt.plot(expected_result_sample_uncropped, '.-')
+plt.plot(input_voltage_camera)
+plt.plot(time, desired_result_camera, '.')
+plt.plot(expected_result_camera_uncropped, '.-')
 plt.grid()
 
 plt.figure()
-plt.plot(input_voltage - naive_input_voltage, '.-')
-plt.plot(expected_result_uncropped - naive_input_voltage, '.-')
+plt.plot(input_voltage_sample - naive_input_voltage_sample, '.-')
+plt.plot(expected_result_sample_uncropped - naive_input_voltage_sample, '.-')
+plt.plot(input_voltage_camera - naive_input_voltage_camera, '.-')
+plt.plot(expected_result_uncropped_camera - naive_input_voltage_camera, '.-')
 plt.grid()
 plt.show()
 
